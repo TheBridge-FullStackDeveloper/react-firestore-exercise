f4c0db917695e39f2ea9e5b482ca5aa159b57f50
Francisco Pérez Muñoz <francisco.perez.munoz@gmail.com>
2024-02-05 21:09:49 +0100

    Project set

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+import { Homepage } from "./pages/Homepage";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<NavBar />}>
+        <Route path="/" element={<Homepage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+export const NavBar = () => {
+  return (
+    <>
+      <div>NavBar</div>
+      <Outlet />
+    </>
+  );
+};
