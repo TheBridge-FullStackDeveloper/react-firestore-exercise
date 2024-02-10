141e083aad317b7788feef85906fbc2ba4dbf4e5
PMFrancisco <francisco.perez.munoz@gmail.com>
2024-02-10 14:10:55 +0100

    AddSong input styled

--- src/pages/AddSong.jsx
+++ src/pages/AddSong.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import { Button, Input } from "@nextui-org/react";
+import React from "react";
 
 export const AddSong = () => {
   return (
-    <div>AddSong</div>
-  )
-}
+    <>
+      <h1>AddSong</h1>
+        <form
+          className="flex flex-col flex-wrap justify-between h-full max-w-screen-md mx-auto"
+          method="post"
+          action="/addSong"
+        >
+          <div className="grid grid-cols-1 px-2 md:grid-cols-2 gap-4">
+            <Input
+              key="title"
+              type="text"
+              label="Title"
+              labelPlacement="outside"
+            />
+
+            <Input
+              key="author"
+              type="text"
+              label="Author"
+              labelPlacement="outside"
+            />
+
+            <Input
+              key="genre"
+              type="text"
+              label="Genre"
+              labelPlacement="outside"
+            />
+
+            <Input
+              key="year"
+              type="number"
+              label="Year"
+              labelPlacement="outside"
+            />
+          </div>
+
+          <Button
+            type="submit"
+            color="primary"
+            variant="ghost"
+            className="self-center my-4"
+          >
+            Submit
+          </Button>
+        </form>
+    </>
+  );
+};
